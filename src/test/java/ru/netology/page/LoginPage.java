6c1042fd213098db704eee16d1e7ae239a5a236e
Elena Levina <qa-levina@yandex.ru>
2022-10-06 23:10:32 +0300

    Add selectors

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -3,10 +3,13 @@ package ru.netology.page;
 import com.codeborne.selenide.SelenideElement;
 import ru.netology.data.DataHelper;
 
+import static com.codeborne.selenide.Selenide.$;
+
 public class LoginPage {
-    private SelenideElement loginField = $();
-    private SelenideElement passwordField = $();
-    private SelenideElement loginButton = $();
+
+    private SelenideElement loginField = $("[data-test-id =login] input");
+    private SelenideElement passwordField = $("[data-test-id=password] input");
+    private SelenideElement loginButton = $("[data-test-id=action-login] input");
 
 
     public VerificationPage validLogin(DataHelper.AuthInfo info){
